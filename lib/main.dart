6f21f65855e68b2c6bf9e89e3165a9f4a81ebb59
Rifai-13 <muhrifai130604@gmail.com>
2025-11-24 20:35:02 +0700

    setup backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:goosen/screens/main_screen.dart';
+// import 'package:goosen/screens/main_screen.dart';
 import './screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+//
 
 
+void main() async {
+  // 3. Pastikan binding initialized sebelum panggil kode native/async
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  // 4. Inisialisasi Firebase dengan opsi dari platform saat ini
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Goosen App',
+      
+      // Tema aplikasi
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green), // Ganti jadi hijau biar sesuai tema Goosen
         useMaterial3: true,
+        // Ganti font default kalau mau, misal GoogleFonts.poppins()
       ),
+      
+      // Hilangkan banner debug di pojok kanan atas
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      
+      // Halaman pertama yang dibuka
+      home: const SplashScreen(),
     );
   }
 }
